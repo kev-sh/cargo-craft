61d7484a3070cb7f062689e957d3b090a3268324
kev-sh <35436653+kev-sh@users.noreply.github.com>
2023-06-04 11:33:59 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("cargo-craft is a work in progress.");
+    println!("The idea is to create a cargo command to scaffold templates of folders and files, for example API endpoints, models, etc.");
+}
